1acc87b8e6d4364675461df8cebb1304e00b9ebe
GNoseda <g.noseda.soto@gmail.com>
2023-01-26 15:51:25 -0300

    course-infor step 1.5

--- src/App.js
+++ src/App.js
@@ -1,45 +1,47 @@
 function App() {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercise: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercise: 7
-    },
-    {
-      name: 'State of a component',
-      exercise: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
   return (
     <div>
       <Header prompt={course}/>
-      <Part parts={parts}/> 
-      <Total parts={parts}/> 
+      <Part prompt={course}/> 
+      <Total prompt={course}/> 
+
     </div>
   )
 }
-
 //One Liner component form
-const Header = (prompt) => <h1>{prompt.prompt}</h1> 
+const Header = ({prompt}) => <h1>{prompt.name}</h1>
 
-const Part = (prompt) => {
-  var text = ""
+const Part = ({prompt}) => {
   return (
     <>
-      <p>{prompt.parts[0].name} {prompt.parts[0].exercise}</p>
-      <p>{prompt.parts[1].name} {prompt.parts[1].exercise}</p>
-      <p>{prompt.parts[2].name} {prompt.parts[2].exercise}</p>
+      <p>{prompt.parts[0].name} {prompt.parts[0].exercises}</p>
+      <p>{prompt.parts[1].name} {prompt.parts[1].exercises}</p>
+      <p>{prompt.parts[2].name} {prompt.parts[2].exercises}</p>
     </>
   )
 }
 
 // Regular component form
-const Total = (prompt) => {
-  let total = prompt.parts[0].exercise + prompt.parts[1].exercise + prompt.parts[2].exercise
+const Total = ({prompt}) => {
+  let total = prompt.parts[0].exercises + prompt.parts[1].exercises + prompt.parts[2].exercises
   return(
     <p>Number of exercises {total} </p>
   )
