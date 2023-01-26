5750cf29b2c9af84edeb1a720d8c78b8f92de3d4
GNoseda <g.noseda.soto@gmail.com>
2023-01-26 15:26:37 -0300

    course-info step 1.4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercise: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercise: 7
+    },
+    {
+      name: 'State of a component',
+      exercise: 14
+    }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header prompt={course}/>
+      <Part parts={parts}/> 
+      <Total parts={parts}/> 
     </div>
-  );
+  )
+}
+
+//One Liner component form
+const Header = (prompt) => <h1>{prompt.prompt}</h1> 
+
+const Part = (prompt) => {
+  var text = ""
+  return (
+    <>
+      <p>{prompt.parts[0].name} {prompt.parts[0].exercise}</p>
+      <p>{prompt.parts[1].name} {prompt.parts[1].exercise}</p>
+      <p>{prompt.parts[2].name} {prompt.parts[2].exercise}</p>
+    </>
+  )
+}
+
+// Regular component form
+const Total = (prompt) => {
+  let total = prompt.parts[0].exercise + prompt.parts[1].exercise + prompt.parts[2].exercise
+  return(
+    <p>Number of exercises {total} </p>
+  )
 }
 
 export default App;
